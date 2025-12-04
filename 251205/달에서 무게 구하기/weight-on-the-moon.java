4152b4bb26f2d667a039b2c25bd53174f6da8208
Ayeong-99 <bay18200725@gmail.com>
2025-12-05 03:06:55 +0900

    Title: 달에서 무게 구하기, Time: 95ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251205/달에서 무게 구하기/weight-on-the-moon.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int weight = 13;
+        double gravity = 0.165000;
+        double multy = weight * gravity;
+
+        System.out.print(weight);
+        System.out.printf(" * %.6f", gravity);
+        System.out.printf(" = %.6f", multy);
+
+
+        // Please write your code here.
+    }
+}
