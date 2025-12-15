49144c8244f4318ddf0dddf5af6052d4ca66e3a4
Ayeong-99 <bay18200725@gmail.com>
2025-12-15 13:27:48 +0900

    Title: 데이터 교환, Time: 90ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251215/데이터 교환/exchange-data.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int a = 5, b = 6, c = 7, temp ;
+        temp = c;
+        c = b;
+        b = a;
+        a = temp;
+        System.out.print(a+"\n"+b+"\n"+c);
+    }
+}
