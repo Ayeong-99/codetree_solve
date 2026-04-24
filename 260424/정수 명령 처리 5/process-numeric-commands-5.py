d02756034be8bd831e3413c9333e34838ab71867
Ayeong-99 <bay18200725@gmail.com>
2026-04-24 22:17:13 +0900

    Title: 정수 명령 처리 5, Time: 0ms, Memory: 0MB, Status: Passed - Codetree

--- 260424/정수 명령 처리 5/process-numeric-commands-5.py
+++ 260424/정수 명령 처리 5/process-numeric-commands-5.py
@@ -13,14 +13,17 @@ for _ in range(N):
 
 # Please write your code here.
 arr = []
+size = 0
 for i in range(N):
     now_command = command[i]
     if now_command == "push_back":
         arr.append(num[i])
+        size += 1 
     elif now_command == "pop_back":
         arr.pop()
+        size -= 1
     elif now_command == "size":
-        print(len(arr))
+        print(size)
     elif now_command == "get":
         geti = num[i] - 1
         print(arr[geti])
