92d85a27d0214f69fe54c089f819a7a676c5d7e6
Ayeong-99 <bay18200725@gmail.com>
2026-06-07 19:56:44 +0900

    [Passed] 코드네임 (49ms, 16MB)

---
+++ trail2/코드네임/code-name.py
@@ -0,0 +1,28 @@
+MAX_N = 5
+
+codenames = []
+scores = []
+for _ in range(MAX_N):
+    codename, score = input().split()
+    codenames.append(codename)
+    scores.append(int(score))
+
+# Please write your code here.
+
+class Student:
+    def __init__(self, codename, score):
+        self.codename = codename
+        self.score = score
+
+students = []   
+min_score = float("inf")
+min_num = -1
+for i in range(MAX_N):
+    if min_score > scores[i]: # 더 작은 점수면 작은 점수 위치로 숫자 바꿔줌
+        min_num = i
+        min_score = scores[i]
+
+    students.append(Student(codenames[i], scores[i]))
+
+answer = students[min_num]
+print(answer.codename, answer.score)
